529db6f9e842d5a72bae585c102be0bd5e0f2e69
hoangphuc552001@gmail.com <hoangphuc552001@gmail.com>
2022-01-13 00:41:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -22,6 +22,39 @@ app.use(express.urlencoded({extended:true}));
 
 
 app.use('/public',express.static('public'))
+app.get("/", async function (req, res) {
+    const highestprice = await productmodel.findTop5ProHighest();
+    const mostbids = await productmodel.findTop5ProMostBids();
+    const instance = await productmodel.findTop5ProInstance();
+    if (req.session.authenticated !==false){
+        const listProductUser=await productmodel.getProductwithUser(req.session.user.id);
+        for (let i=0;i<highestprice.length;i++){
+            highestprice[i].authenticated=req.session.authenticated;
+            mostbids[i].authenticated=req.session.authenticated;
+            instance[i].authenticated=req.session.authenticated;
+            if (highestprice[i].holder===+req.session.user.id) highestprice[i].holderBid=true
+            if (mostbids[i].holder===+req.session.user.id) mostbids[i].holderBid=true
+            if (instance[i].holder===+req.session.user.id) instance[i].holderBid=true
+            for (let j=0;j<listProductUser.length;j++){
+                if (listProductUser[j].product===highestprice[i].id){
+                    highestprice[i].checkwl=true;
+                    mostbids[i].checkwl=true;
+                    instance[i].checkwl=true;
+                }
+            }
+        }
+    }
+    res.render("index", {
+        highestprice,
+        mostbids,
+        instance,
+        activeHome:true,
+        empty:
+            mostbids.length === 0 ||
+            highestprice.length === 0 ||
+            instance.length === 0,
+    });
+})
 // activate_session_middleware(app);
 // activate_locals_middleware(app)
 //
