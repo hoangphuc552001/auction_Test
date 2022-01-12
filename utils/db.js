20440bddf7a6e13133683d1d85cc89f9afb7d1ad
hoangphuc552001@gmail.com <hoangphuc552001@gmail.com>
2022-01-12 23:50:37 +0700

    up

--- utils/db.js
+++ utils/db.js
@@ -2,10 +2,9 @@ import fn from "knex";
 const knex = fn({
     client: 'mysql2',
     connection: {
-        host : '127.0.0.1',
-        port : 3306,
-        user : 'root',
-        password : '',
+        host : 'us-cdbr-east-05.cleardb.net',
+        user : 'be44caddfce4b6',
+        password : '00bbb032',
         database : 'heroku_7067838459c52ba'
     },
     pool: { min: 0, max: 10 },
--- utils/db2.js
+++ utils/db2.js
@@ -2,10 +2,9 @@ import knex from "./db";
 
 let mysql = require('mysql');
 let config = {
-    host : '127.0.0.1',
-    port : 3306,
-    user : 'root',
-    password : '',
+    host : 'us-cdbr-east-05.cleardb.net',
+    user : 'be44caddfce4b6',
+    password : '00bbb032',
     database : 'heroku_7067838459c52ba'
 };
 let connection = mysql.createConnection(config);
